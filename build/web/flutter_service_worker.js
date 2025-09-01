be8d27a210a900791953dc92f591e05012dab446
Sagar-S-R <sagarsr2006@gmail.com>
2025-09-01 20:22:19 +0530

    build-2

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -46,7 +46,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "cf678f16fce1dd32c9d9dc351faba0ec
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "98c5af52b41cc2dbebf7fd15c93e3bc9",
+"flutter_bootstrap.js": "354ba1f62e288c10b139ade25874b9cc",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
